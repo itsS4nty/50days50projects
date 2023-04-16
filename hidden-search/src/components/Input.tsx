59251b3636398a992e40037309e47ee089bcfd10
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2023-04-16 23:56:56 +0200

    Finish 'Hidden search' project

--- hidden-search/src/components/Input.tsx
+++ hidden-search/src/components/Input.tsx
@@ -3,43 +3,45 @@ import styled from 'styled-components';
 import SearchSVG from '../svg/SearchSVG';
 
 const Container = styled.div<{ isOpen: boolean }>`
-	width: ${props => props.isOpen ? '256px' : '48px'};
-    height: 48px;
+	width: ${(props) => (props.isOpen ? '256px' : '48px')};
+	height: 48px;
 	position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: width .25s linear;
-    
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	transition: width 0.25s linear;
+
 	input {
-        width: 100%;
-        height: inherit;
-        border-radius: 12px;
-        padding-left: 15px;
-        font-size: 18px;
+		width: 100%;
+		height: inherit;
+		border-radius: 12px;
+		padding-left: 15px;
+		font-size: 18px;
 	}
-    
+
 	span {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		position: absolute;
-        top: 0;
-        right: 0;
-        cursor: pointer;
-        background: white;
-        width: 48px;
-        height: inherit;
-        border-radius: 12px;
+		top: 0;
+		right: 0;
+		cursor: pointer;
+		background: white;
+		width: 48px;
+		height: inherit;
+		border-radius: 12px;
 	}
 `;
 
 const Input = () => {
-    const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 	return (
 		<Container isOpen={open}>
 			<input type='text' placeholder='Search...' />
-			<span onClick={() => setOpen(!open)}><SearchSVG /></span>
+			<span onClick={() => setOpen(!open)}>
+				<SearchSVG />
+			</span>
 		</Container>
 	);
 };
